495fab2cf9daba3a5c3a7b857ca25a04a288342f
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-11-08 16:04:54 +0100

    deleted node modules

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import { concatenation } from "./concatenation";
 const button = document.querySelector("button");
 const input = document.querySelector("input");
+console.log(input);
 if (button && input) {
     button.addEventListener("click", () => {
         concatenation(input.value, "hello!");
